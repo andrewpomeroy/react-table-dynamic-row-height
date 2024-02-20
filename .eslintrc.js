cac7ac75fb8cc2fa6e29f67f42f40edb466b8689
Andrew Pomeroy <andrewmichaelpomeroy@gmail.com>
2024-02-19 20:43:04 -0800

    lint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+const react = require("eslint-plugin-react");
+const typescript = require("@typescript-eslint/eslint-plugin");
+// const globals = require("globals");
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  // plugins: [
+  //   react,
+  //   typescript,
+  // ],
+  settings: {
+    react: {
+      createClass: "createReactClass", // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: "React", // Pragma to use, default to "React"
+      fragment: "Fragment", // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // It will default to "latest" and warn if missing, and to "detect" in the future
+      flowVersion: "0.53", // Flow version
+    },
+  },
+  rules: {},
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
+import "./index.css";
 
 import {
   ColumnDef,
@@ -10,11 +10,11 @@ import {
   getSortedRowModel,
   Row,
   useReactTable,
-} from '@tanstack/react-table';
+} from "@tanstack/react-table";
 
-import { useVirtualizer } from '@tanstack/react-virtual';
+import { useVirtualizer } from "@tanstack/react-virtual";
 
-import { makeData, Person } from './makeData';
+import { makeData, Person } from "./makeData";
 
 //This is a dynamic row height example, which is more complicated, but allows for a more realistic table.
 //See https://tanstack.com/virtual/v3/docs/examples/react/table for a simpler fixed row height example.
@@ -23,42 +23,42 @@ function App() {
   const columns = React.useMemo<ColumnDef<Person>[]>(
     () => [
       {
-        accessorKey: 'id',
-        header: 'ID',
+        accessorKey: "id",
+        header: "ID",
         size: 60,
       },
       {
-        accessorKey: 'firstName',
+        accessorKey: "firstName",
         cell: (info) => info.getValue(),
       },
       {
         accessorFn: (row) => row.lastName,
-        id: 'lastName',
+        id: "lastName",
         cell: (info) => info.getValue(),
         header: () => <span>Last Name</span>,
       },
       {
-        accessorKey: 'age',
-        header: () => 'Age',
+        accessorKey: "age",
+        header: () => "Age",
         size: 50,
       },
       {
-        accessorKey: 'visits',
+        accessorKey: "visits",
         header: () => <span>Visits</span>,
         size: 50,
       },
       {
-        accessorKey: 'status',
-        header: 'Status',
+        accessorKey: "status",
+        header: "Status",
       },
       {
-        accessorKey: 'progress',
-        header: 'Profile Progress',
+        accessorKey: "progress",
+        header: "Profile Progress",
         size: 80,
       },
       {
-        accessorKey: 'createdAt',
-        header: 'Created At',
+        accessorKey: "createdAt",
+        header: "Created At",
         cell: (info) => info.getValue<Date>().toLocaleString(),
         size: 250,
       },
@@ -66,7 +66,7 @@ function App() {
     []
   );
 
-  const [data, _setData] = React.useState(() => makeData(50_000));
+  const [data] = React.useState(() => makeData(50_000));
 
   // const dataWithExpandedRow = selectedIndex !== null ?
 
@@ -89,8 +89,8 @@ function App() {
     getScrollElement: () => tableContainerRef.current,
     //measure dynamic row height, except in firefox because it measures table border height incorrectly
     measureElement:
-      typeof window !== 'undefined' &&
-      navigator.userAgent.indexOf('Firefox') === -1
+      typeof window !== "undefined" &&
+      navigator.userAgent.indexOf("Firefox") === -1
         ? (element) => element?.getBoundingClientRect().height
         : undefined,
     overscan: 100,
@@ -99,7 +99,7 @@ function App() {
   //All important CSS styles are included as inline styles for this example. This is not recommended for your code.
   return (
     <div className="app">
-      {process.env.NODE_ENV === 'development' ? (
+      {process.env.NODE_ENV === "development" ? (
         <p>
           <strong>Notice:</strong> You are currently running React in
           development mode. Virtualized rendering performance will be slightly
@@ -111,17 +111,17 @@ function App() {
         className="container"
         ref={tableContainerRef}
         style={{
-          overflow: 'auto', //our scrollable table container
-          position: 'relative', //needed for sticky header
-          height: '800px', //should be a fixed height
+          overflow: "auto", //our scrollable table container
+          position: "relative", //needed for sticky header
+          height: "800px", //should be a fixed height
         }}
       >
-        {/* Even though we're still using sematic table tags, we must use CSS grid and flexbox for dynamic row heights */}
-        <div style={{ display: 'grid' }}>
+        {/* we must use CSS grid and flexbox for dynamic row heights */}
+        <div style={{ display: "grid" }} role="table">
           <div
             style={{
-              display: 'grid',
-              position: 'sticky',
+              display: "grid",
+              position: "sticky",
               top: 0,
               zIndex: 1,
             }}
@@ -129,22 +129,23 @@ function App() {
             {table.getHeaderGroups().map((headerGroup) => (
               <div
                 key={headerGroup.id}
-                style={{ display: 'flex', width: '100%' }}
+                style={{ display: "flex", width: "100%" }}
               >
                 {headerGroup.headers.map((header) => {
                   return (
                     <div
                       key={header.id}
                       style={{
-                        display: 'flex',
+                        display: "flex",
                         width: header.getSize(),
                       }}
+                      role="columnheader"
                     >
                       <div
                         {...{
                           className: header.column.getCanSort()
-                            ? 'cursor-pointer select-none'
-                            : '',
+                            ? "cursor-pointer select-none"
+                            : "",
                           onClick: header.column.getToggleSortingHandler(),
                         }}
                       >
@@ -153,8 +154,8 @@ function App() {
                           header.getContext()
                         )}
                         {{
-                          asc: ' 🔼',
-                          desc: ' 🔽',
+                          asc: " 🔼",
+                          desc: " 🔽",
                         }[header.column.getIsSorted() as string] ?? null}
                       </div>
                     </div>
@@ -165,9 +166,9 @@ function App() {
           </div>
           <div
             style={{
-              display: 'grid',
+              display: "grid",
               height: `${rowVirtualizer.getTotalSize()}px`, //tells scrollbar how big the table is
-              position: 'relative', //needed for absolute positioning of rows
+              position: "relative", //needed for absolute positioning of rows
             }}
           >
             {rowVirtualizer.getVirtualItems().map((virtualRow, index) => {
@@ -196,10 +197,10 @@ function App() {
                     ref={(node) => rowVirtualizer.measureElement(node)} //measure dynamic row height
                     key={row.id}
                     style={{
-                      display: 'flex',
-                      position: 'absolute',
+                      display: "flex",
+                      position: "absolute",
                       transform: `translateY(${virtualRow.start}px)`, //this should always be a `style` as it changes on scroll
-                      width: '100%',
+                      width: "100%",
                       fontSize:
                         virtualRow.index === selectedIndex ? 32 : undefined,
                     }}
@@ -234,9 +235,10 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById('root');
 
-if (!rootElement) throw new Error('Failed to find the root element');
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("Failed to find the root element");
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
